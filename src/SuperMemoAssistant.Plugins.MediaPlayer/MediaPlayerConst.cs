12fa812f4b73c1cd54ee36aa3ba789624c95f911
bjsi <experimentallearning0@gmail.com>
2021-01-05 17:31:50 +0000

    - Added: Media Element HTML elements and regexes, other constants

---
+++ src/SuperMemoAssistant.Plugins.MediaPlayer/MediaPlayerConst.cs
@@ -0,0 +1,35 @@
+using System.Text.RegularExpressions;
+using AdvancedMediaPlayer.Models;
+
+namespace AdvancedMediaPlayer
+{
+    internal static class AMPConst
+    {
+
+        #region Constants & Statics
+        // HTML snippets here are formatted in the final form they should assume in SM.
+
+        //
+        // Elements with locally saved video data
+        public const string LocalElementFormat = @"<DIV id=media-player-local-element-title>{0}</DIV>
+<DIV id=media-player-local-element-filename>{1}</DIV>
+<DIV id=media-player-local-element-data>{2}</DIV>";
+        public const string LocalElementDataFormat = "<DIV id=media-player-local-element-data>{0}</DIV>";
+
+        public static readonly Regex RE_LocalElement = new Regex("<DIV id=media-player-local-element-data>([^<]+)</DIV>", RegexOptions.IgnoreCase);
+
+        //
+        // Elements with data from youtube 
+        public const string YouTubeElementFormat = @"<DIV id=media-player-yt-element-title>{0}</DIV>
+<DIV id=media-player-yt-element-filename>{1}</DIV>
+<DIV id=media-player-yt-element-data>{2}</DIV>";
+        public const string YouTubeElementDataFormat = "<DIV id=media-player-yt-element-data>{0}</DIV>";
+
+        public static readonly Regex RE_YouTubeElement = new Regex("<DIV id=media-player-yt-element-data>([^<]+)</DIV>", RegexOptions.IgnoreCase);
+
+        public const double DefaultExtractPriority = 15.0;
+
+        public const ViewMode DefaultViewMode = ViewMode.Normal;
+        #endregion
+    }
+}
